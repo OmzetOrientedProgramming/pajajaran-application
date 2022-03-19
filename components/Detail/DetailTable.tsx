2a6ae3516edc13e4ec1911af54bdfc585b873738
Antonius Anggito Arissaputro <antonius.anggito@ui.ac.id>
2022-03-19 18:40:08 +0700

    [REFACTOR] Add DetailTable component

--- components/Detail/DetailTable.tsx
+++ components/Detail/DetailTable.tsx
@@ -1,20 +1,101 @@
 import React from 'react';
 import 'twin.macro';
+import tw from 'twin.macro';
 
-interface DetailTableProps {
-  children: React.ReactNode;
-  title: string;
+const statusMap: Map<number, string> = new Map([
+  [0, 'Menunggu Konfirmasi'],
+  [1, 'Menunggu Pembayaran'],
+  [2, 'Diterima'],
+  [3, 'Selesai'],
+]);
+
+interface TitleProps {
+  children: string;
 }
 
-const DetailTable: React.FC<DetailTableProps> = ({ children, title }) => {
+interface InfoContentProps {
+  id: number;
+  date: string;
+  start_time: string;
+  end_time: string;
+  capacity: number;
+  status: number;
+}
+
+interface ItemContentProps {
+  items: any;
+}
+
+const Title: React.FunctionComponent<TitleProps> = (props) => (
+  <div tw="py-3 rounded-t-lg background[#003366] flex justify-center items-center w-full">
+    <h2 tw="font-bold text-2xl color[#FFFFFF]">{props.children}</h2>
+  </div>
+);
+
+const InfoContent: React.FunctionComponent<InfoContentProps> = (props) => {
+  const fields = [
+    { title: 'Nama Customer', value: props.id },
+    { title: 'Jumlah Pengunjung', value: props.capacity },
+    { title: 'Tanggal Booking', value: props.date },
+    { title: 'Jam Booking', value: `${props.start_time}-${props.end_time}` },
+    { title: 'Status Booking', value: statusMap.get(props.status) },
+  ];
+
   return (
-    <div tw="">
-      <div tw="background[#003366] flex justify-center items-center">
-        <h2 tw="font-bold text-2xl color[#FFFFFF]">{title}</h2>
-      </div>
-      <div tw="">{children}</div>
+    <div tw="py-4">
+      {fields.map((field: any, key: any) => (
+        <div key={key} tw="px-12">
+          <div
+            css={[
+              tw`flex flex-row justify-between items-center border-b-2 px-8 py-3`,
+              field.title === 'Status Booking' && tw`font-bold border-none`,
+            ]}
+          >
+            <p tw="text-xl color[#003366] ">{field.title}</p>
+            <p tw="text-xl color[#003366]">{field.value}</p>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
+
+const ItemContent: React.FunctionComponent<ItemContentProps> = (props) => {
+  return (
+    <div tw="py-4">
+      {props.items.map((item: any, key: any) => (
+        <div key={key} tw="px-12">
+          <div
+            css={[
+              tw`flex flex-row justify-between items-center border-b px-8 py-3`,
+              item.title === 'Status Booking' && tw`font-bold border-none`,
+            ]}
+          >
+            <img src={item.image} alt={item.name} tw="w-[60px] h-auto" />
+            <p tw="text-xl color[#003366]">{item.name}</p>
+            <p tw="text-xl color[#003366]">{item.price}</p>
+            <p tw="text-xl color[#003366]">{item.qty}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+interface DetailTableSubComponents {
+  Title: React.FunctionComponent<TitleProps>;
+  InfoContent: React.FunctionComponent<InfoContentProps>;
+  ItemContent: React.FunctionComponent<ItemContentProps>;
+}
+
+const DetailTable: React.FunctionComponent & DetailTableSubComponents = (
+  props
+) => {
+  return <div tw="w-[800px] border rounded-b-lg" {...props} />;
+};
+
+DetailTable.Title = Title;
+DetailTable.InfoContent = InfoContent;
+DetailTable.ItemContent = ItemContent;
 
 export default DetailTable;
--- pages/booking/[id].tsx
+++ pages/booking/[id].tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import 'twin.macro';
 import Head from 'next/head';
 import { Layout } from '../../components/Utils/Layout';
-import DetailTable from '../../components/Detail/DetailTable';
 import { dummyGetDetailBookingResponse as dataMock } from '../../__mocks__/apis/detailMocks';
 import DetailRingkasan from '../../components/Detail/DetailRingkasan';
 import { css } from 'twin.macro';
+import DetailTable from '../../components/Detail/DetailTable';
 
 const Detail: React.FC = () => {
   return (
@@ -24,19 +24,28 @@ const Detail: React.FC = () => {
             <h3 tw="font-bold text-xl color[#003366]">
               Id Booking {dataMock.id}
             </h3>
-            <p tw="color[#829CB6]">Dibuat pada {dataMock.date}</p>
+            <p tw="color[#829CB6]">Dibuat pada {dataMock.created_at}</p>
           </div>
         </div>
-        <div tw="flex flex-row justify-around items-center">
-          <div tw="flex flex-col justify-center items-center">
-            <div tw="">
-              <DetailTable title="Informasi Booking">
-                <div>Booking</div>
+        <div tw="flex flex-row justify-between items-center">
+          <div id="info" tw="flex flex-col justify-center items-center">
+            <div tw="mb-5">
+              <DetailTable>
+                <DetailTable.Title>Informasi Booking</DetailTable.Title>
+                <DetailTable.InfoContent
+                  id={dataMock.id}
+                  date={dataMock.date}
+                  start_time={dataMock.start_time}
+                  end_time={dataMock.end_time}
+                  capacity={dataMock.capacity}
+                  status={dataMock.status}
+                />
               </DetailTable>
             </div>
             <div tw="">
-              <DetailTable title="Pesanan Tambahan">
-                <div>Item</div>
+              <DetailTable>
+                <DetailTable.Title>Pesanan Tambahan</DetailTable.Title>
+                <DetailTable.ItemContent items={dataMock.items} />
               </DetailTable>
             </div>
           </div>
