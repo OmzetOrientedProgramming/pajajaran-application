572a0902a539fa51a2fff0c7af31cd0853d3b158
Antonius Anggito Arissaputro <antonius.anggito@ui.ac.id>
2022-03-29 19:09:19 +0700

    [RED] Create confirmBooking integrating test

--- __tests__/pages/detailBookingRequest.test.tsx
+++ __tests__/pages/detailBookingRequest.test.tsx
@@ -1,4 +1,10 @@
-import { cleanup, render, screen, waitFor } from '@testing-library/react';
+import {
+  cleanup,
+  fireEvent,
+  render,
+  screen,
+  waitFor,
+} from '@testing-library/react';
 import axios from 'axios';
 
 import { headers } from '../../apis/constants';
@@ -6,7 +12,9 @@ import endpoint from '../../apis/endpoint';
 import DetailBooking from '../../pages/booking/[id]';
 import ExampleWrapper from '../../__mocks__/pages/example';
 import {
+  confirmBookingParams,
   getDetailBookingParams,
+  mockConfirmBookingResponse,
   mockGetDetailBookingResponse,
 } from '../../__mocks__/apis/detailBookingMocks';
 
@@ -62,6 +70,49 @@ describe('useGetDetailBooking()', () => {
       expect(mockAxios.get).toHaveBeenCalledTimes(1);
       expect(screen.queryByText('22 April 2022')).not.toBeInTheDocument();
       expect(screen.queryByText('Kopi Mocha Asik')).not.toBeInTheDocument();
+    });
+  });
+});
+
+describe('useConfirmBooking()', () => {
+  test('confirmation button request works correctly', async () => {
+    mockAxios.patch.mockResolvedValueOnce(mockConfirmBookingResponse);
+
+    render(
+      <ExampleWrapper>
+        <DetailBooking />
+      </ExampleWrapper>
+    );
+
+    fireEvent.click(screen.getByText('Terima'));
+
+    expect(mockAxios.patch).toHaveBeenCalledTimes(1);
+    expect(mockAxios.patch).toHaveBeenCalledWith(
+      `${endpoint.detailBooking}/${confirmBookingParams.id}/confirmation`,
+      { booking_status: confirmBookingParams.booking_status },
+      { headers: headers }
+    );
+
+    await waitFor(() => {
+      expect(screen.queryByText('Terima')).not.toBeInTheDocument();
+    });
+  });
+
+  test('confirmation button failed request', async () => {
+    mockAxios.patch.mockReturnValueOnce(Promise.reject(new Error('get error')));
+
+    render(
+      <ExampleWrapper>
+        <DetailBooking />
+      </ExampleWrapper>
+    );
+
+    fireEvent.click(screen.getByText('Terima'));
+
+    // await expect(mockAxios.get).rejects.toThrow('get error');
+    await waitFor(() => {
+      expect(mockAxios.patch).toHaveBeenCalledTimes(1);
+      expect(screen.getByText('Terima')).toBeInTheDocument();
     });
   });
 });
