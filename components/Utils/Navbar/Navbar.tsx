3a0d122953fb741797619d17c5409a45c0d6ccc4
Mario Serano <mario.serano@ui.ac.id>
2022-04-02 15:28:23 +0700

    Added navbar to web admin

---
+++ components/Utils/Navbar/Navbar.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { css } from 'twin.macro';
+
+import Link from 'next/link';
+
+export type NavbarItem = {
+  text: string;
+  href: string;
+};
+
+interface NavbarProps {
+  navbarItems: Array<NavbarItem>;
+}
+
+const Navbar: React.FC<NavbarProps> = (props) => {
+  const { navbarItems } = props;
+  return (
+    <div
+      tw="h-16 w-full fixed top-0 left-0"
+      css={css`
+        box-shadow: 0px 3px 16px 0px #00000012;
+      `}
+    >
+      <div
+        tw="flex justify-center py-5 px-5 max-w-none md:(px-8 mx-auto max-w-3xl) 
+      lg:(px-28 max-w-5xl) xl:(px-20 max-w-7xl) 2xl:(px-40 max-w-full)"
+      >
+        <div tw="w-full flex justify-between items-center">
+          <Link href="/">
+            <a tw="block cursor-pointer">
+              <img tw="h-6 w-auto" src="/images/wave.png" alt="Logo" />
+            </a>
+          </Link>
+          <div tw="flex space-x-10">
+            {navbarItems.map((navbarItem, key) => (
+              <Link href={navbarItem.href} key={key}>
+                <a tw="block cursor-pointer">{navbarItem.text}</a>
+              </Link>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
